5629c6449baf18bea43d3b200352eb6cd5a46078
Nikola0707 <ristoski.nikola@gmail.com>
2023-01-15 16:54:47 +0100

    work with unbdefined and null and does not return more than 3 matches

--- script.test.js
+++ script.test.js
@@ -6,11 +6,23 @@ dbMock = [
     'dogpictures.com'
 ]
 
-it('is searching google to return empty array if string not included', () => {
-    expect(googleSearch('testtest', dbMock)).toEqual([])
+describe('googleSearch', () => {
+    it('is searching google to return empty array if string not included', () => {
+        expect(googleSearch('testtest', dbMock)).toEqual([])
 
+    })
+
+    it('is searching google to return data if string included', () => {
+        expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com'])
+    })
+
+    it('work with undefined and null input', () => {
+        expect(googleSearch(undefined, dbMock)).toEqual([])
+        expect(googleSearch(null, dbMock)).toEqual([])
+    })
+
+    it('doesn not return more than 3 matches', () => {
+        expect(googleSearch('.com', dbMock).length).toEqual(3)
+    })
 })
 
-it('is searching google to return data if string included', () => {
-    expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com'])
-})
